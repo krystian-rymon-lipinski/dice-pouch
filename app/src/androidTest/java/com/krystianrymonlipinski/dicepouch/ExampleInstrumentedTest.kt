88c89899d57cd4ce033ff42fa097036debad2038
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2023-08-17 16:18:17 +0200

    Revert to the previous tool; check with failing tests

--- app/src/androidTest/java/com/krystianrymonlipinski/dicepouch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/krystianrymonlipinski/dicepouch/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.krystianrymonlipinski.dicepouch", appContext.packageName)
+        assertEquals("com.krystianrymonlipinski.dicepouch.something_purposefully_wrong", appContext.packageName)
     }
 }
