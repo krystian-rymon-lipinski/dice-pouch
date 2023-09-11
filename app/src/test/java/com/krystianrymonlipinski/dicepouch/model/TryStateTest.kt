6ede4945c83fa82a3664b27987971550dd438742
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2023-09-11 12:22:24 +0200

    Write unit tests for TryState model class

---
+++ app/src/test/java/com/krystianrymonlipinski/dicepouch/model/TryStateTest.kt
@@ -0,0 +1,50 @@
+package com.krystianrymonlipinski.dicepouch.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class TryStateTest {
+
+    private lateinit var testObj: TryState
+
+    @Before
+    fun setUp() {
+        testObj = TryState(
+            throws = listOf(3, 2, null),
+            result = null,
+            isChosen = false
+        )
+    }
+
+    @Test
+    fun updateWithNewThrow() {
+        testObj = testObj.updateWithNewThrow(2, 10)
+        assertEquals(10, testObj.throws[2])
+    }
+
+    @Test
+    fun updateResult_whenAnyThrowIsNull() {
+        testObj = testObj.updateResult(2)
+        assertNull(testObj.result)
+    }
+
+    @Test
+    fun updateResult_whenNoThrowIsNull() {
+        testObj = testObj
+            .copy(throws = listOf(3, 2, 7))
+            .updateResult(-1)
+        assertEquals(11, testObj.result)
+    }
+
+    @Test
+    fun updateIsChosen() {
+        testObj = testObj.updateAsChosen()
+        assertTrue(testObj.isChosen)
+    }
+}
