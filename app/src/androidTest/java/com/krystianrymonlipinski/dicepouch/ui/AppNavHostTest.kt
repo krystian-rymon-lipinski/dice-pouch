275c2b03ba02d967c40bd705aa2dc170a298ca66
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2023-11-14 15:49:55 +0100

    Fix navigation instrumental tests

--- app/src/androidTest/java/com/krystianrymonlipinski/dicepouch/ui/AppNavHostTest.kt
+++ app/src/androidTest/java/com/krystianrymonlipinski/dicepouch/ui/AppNavHostTest.kt
@@ -1,13 +1,21 @@
 package com.krystianrymonlipinski.dicepouch.ui
 
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.longClick
+import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onParent
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTouchInput
 import com.krystianrymonlipinski.dicepouch.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
 
@@ -23,21 +31,88 @@ class AppNavHostTest {
 
     @Test
     fun appNavigation_startDestination() {
-        composeTestRule.onNodeWithText("Basic D&D Set").assertIsDisplayed()
+        checkIfRollScreenOn()
     }
 
     @Test
-    fun appNavigation_navigateToAndFrom_diceSetEditScreen() {
+    fun appNavigation_navigateThroughTabs() {
         composeTestRule.apply {
-            onNodeWithContentDescription("edit_set_icon").performClick()
-            onNodeWithText("Basic D&D Set").assertDoesNotExist()
+            onNodeWithText("Pouch").performClick()
+            checkIfPouchScreenOn()
 
             activityRule.scenario.onActivity {
                 it.onBackPressedDispatcher.onBackPressed()
             }
-            onNodeWithText("Basic D&D Set").assertIsDisplayed()
+            checkIfRollScreenOn()
+
+            onNodeWithText("Pouch").performClick()
+            checkIfPouchScreenOn()
+
+            onNodeWithText("Table").performClick()
+            checkIfRollScreenOn()
         }
+    }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun appNavigation_navigateToEditSetScreen_andBack_byOnBackPressed() = runTest {
+        composeTestRule.apply {
+            onNodeWithText("Pouch").performClick()
+            onNodeWithContentDescription("edit_set").assertDoesNotExist()
 
+            onNodeWithText(text = "Basic set", useUnmergedTree = true).onParent().performTouchInput { longClick() }
+            awaitIdle()
+
+            onNodeWithContentDescription("edit_set").assertIsDisplayed()
+
+            onNodeWithContentDescription("edit_set").performClick()
+            checkIfDiceSetEditScreenOn()
+
+            activityRule.scenario.onActivity {
+                it.onBackPressedDispatcher.onBackPressed()
+            }
+            checkIfPouchScreenOn()
+        }
+    }
+
+    @Test
+    fun appNavigation_navigateToEditSetScreen_andBack_byUp() {
+        composeTestRule.apply {
+            onNodeWithText("Pouch").performClick()
+            onNodeWithContentDescription("edit_set").assertDoesNotExist()
+
+            onNodeWithText("Basic set").performTouchInput { longClick(durationMillis = 1000L) }
+            onNodeWithContentDescription("edit_set").assertIsDisplayed()
+
+            onNodeWithContentDescription("edit_set").performClick()
+            checkIfDiceSetEditScreenOn()
+
+            onNodeWithContentDescription("arrow_back").performClick()
+            checkIfPouchScreenOn()
+        }
+    }
+
+    private fun checkIfRollScreenOn() {
+        composeTestRule.apply {
+            onNodeWithText("Table")
+                .assertIsDisplayed()
+                .assertIsSelected()
+        }
+    }
+
+    private fun checkIfPouchScreenOn() {
+        composeTestRule.apply {
+            onNodeWithText("Pouch")
+                .assertIsDisplayed()
+                .assertIsSelected()
+        }
+    }
+
+    private fun checkIfDiceSetEditScreenOn() {
+        composeTestRule.apply {
+            onNodeWithContentDescription("arrow_back").assertIsDisplayed()
+            onNodeWithText("Basic set").assertIsDisplayed()
+            onAllNodesWithContentDescription("add_icon").assertCountEquals(2)
+        }
     }
 }
