13e399877ab1bdabc9b5422737e55a91374be050
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2023-12-05 18:14:05 +0100

    Fix SettingsScreen UI test (v2)

--- app/src/androidTest/java/com/krystianrymonlipinski/dicepouch/ui/screens/SettingsScreenTest.kt
+++ app/src/androidTest/java/com/krystianrymonlipinski/dicepouch/ui/screens/SettingsScreenTest.kt
@@ -1,7 +1,7 @@
 package com.krystianrymonlipinski.dicepouch.ui.screens
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.krystianrymonlipinski.dicepouch.BaseAndroidTest
@@ -25,14 +25,12 @@ class SettingsScreenTest : BaseAndroidTest() {
     @Test
     fun checkAutodismissRollPopup() {
         composeTestRule.apply {
-            onNodeWithTag(testTag = "popup_autodismiss_switch").performClick()
+            onNodeWithContentDescription("popup_autodismiss_switch").performClick()
             onNodeWithText(text = "after finishing roll", substring = true).assertIsDisplayed()
 
-            onNodeWithTag(testTag = "popup_autodismiss_switch").performClick()
+            onNodeWithContentDescription("popup_autodismiss_switch").performClick()
             onNodeWithText(text = "after finishing roll", substring = true).assertDoesNotExist()
         }
-
     }
 
-    //TODO: improve; write tests for sliders
 }
