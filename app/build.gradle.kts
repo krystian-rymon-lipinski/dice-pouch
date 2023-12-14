6ea1e9163663c8a6be24131889ef0a2c8c93fe68
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2023-12-14 09:43:01 +0100

    Bump up version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "com.krystianrymonlipinski.dicepouch"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "com.krystianrymonlipinski.dicepouch.CustomTestRunner"
